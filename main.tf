ec3348f3f4e2d8ec1cfbe20709e16b026dc67291
jluke_r1github <jluke@r1rcm.com>
2022-05-04 16:05:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,32 +35,18 @@ resource "snowflake_schema" "schema" {
 
 # Create Table
 
-resource "snowflake_sequence" "sequence" {
-  database = snowflake_schema.schema.database
-  schema   = snowflake_schema.schema.name
-  name     = "sequence"
-}
 
 resource "snowflake_table" "table" {
   database            = snowflake_schema.schema.database
   schema              = snowflake_schema.schema.name
-  name                = "table"
+  name                = "TFTable"
   comment             = "A table."
-  cluster_by          = ["to_date(DATE)"]
-  data_retention_days = snowflake_schema.schema.data_retention_days
-  change_tracking     = false
 
   column {
     name     = "id"
     type     = "int"
     nullable = true
-
-    default {
-      sequence = snowflake_sequence.sequence.fully_qualified_name
-    }
   }
-
-  
 
   column {
     name     = "data"
@@ -77,10 +63,5 @@ resource "snowflake_table" "table" {
     name    = "extra"
     type    = "VARIANT"
     comment = "extra data"
-  }
-
-  primary_key {
-    name = "my_key"
-    keys = ["data"]
   }
 }
